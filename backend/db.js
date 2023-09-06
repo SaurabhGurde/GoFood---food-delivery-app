be91dcccb1a2a0d8fbee9eb6406bab66e8b8f575
Saurabh <saurabhgurde@gmail.com>
2023-09-06 17:41:01 +0530

    first

---
+++ backend/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const connectionString = ('mongodb+srv://gofood:gofood5578@cluster0.cyt05sw.mongodb.net/gofoodmern?retryWrites=true&w=majority');
+async function mongoDB() {
+  try {
+    await mongoose.connect(connectionString,{useNewUrlParser: true});
+    console.log('Connected to MongoDB');
+    const fetched_data = await mongoose.connection.db.collection("food_items");
+    const data = await fetched_data.find({}).toArray()
+    const foodCategory = await mongoose.connection.db.collection("foodCategory");
+    const catData = await foodCategory.find({}).toArray()             
+      global.foodData = data;
+      global.foodCategory = catData;
+    
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+  }
+}
+
+
+module.exports = mongoDB; 
