9fa5576cdb7452bab4bd414fdf5c6388f9b98164
Saurabh <saurabhgurde@gmail.com>
2023-09-26 18:53:19 +0530

    eighth

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoDB = require('./db')
+const cors = require('cors')
 mongoDB();
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
