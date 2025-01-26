8d2db55c1682a63702dc00b79a1dcc81d4da65d4
Saurabh Gurde <saurabhgurde@gmail.com>
2025-01-27 01:44:01 +0530

    post method error solve

--- backend/api/index.js
+++ backend/api/index.js
@@ -12,14 +12,15 @@ const port = 5000;
 const mongoDB = require("../db");
 const cors = require("cors");
 mongoDB();
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use(bodyParser.json({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
